86797d23c1d769f0c784b761a864010eb61ed870
Yuge <yuge.a.24kdgh@gmail.com>
2025-11-13 21:27:41 +0900

    vite変更

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,12 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+//   base: process.env.NODE_ENV === 'production' ? '/portfolio' : '/'
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: process.env.NODE_ENV === 'production' ? '/portfolio' : '/'
+  base: '/portfolio/'  
 })
