1276f3578258e51968749e1e3c0d18ec3e7186da
Yuge <yuge.a.24kdgh@gmail.com>
2025-11-13 21:10:24 +0900

    app変更

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   // // デバックモードなので無視でおけ
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename="/portfolio">
       <App />
     </BrowserRouter>
   </StrictMode>
