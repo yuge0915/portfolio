7f5cc4be4b3fdb0bc342cd401467b5964b2c8817
Yuge <yuge.a.24kdgh@gmail.com>
2025-11-13 21:20:21 +0900

    main

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'  // ← 変更
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  // // デバックモードなので無視でおけ
   <StrictMode>
-    <BrowserRouter basename="/">
+    <HashRouter>  {/* ← 変更（basename削除） */}
       <App />
-    </BrowserRouter>
+    </HashRouter>  {/* ← 変更 */}
   </StrictMode>
 )
